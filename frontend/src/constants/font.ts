64bc8a8b745137930253a98979f1d57ca8d1d4e9
hhw12409 <sksk2425@naver.com>
2022-07-03 15:09:28 +0900

    Feat : constats module

---
+++ frontend/src/constants/font.ts
@@ -0,0 +1,20 @@
+const FONT_WEIGHT = {
+  NORMAL: 400,
+  BOLD: 700,
+  EXTRA_BOLD: 800,
+}
+
+const FONT_SIZE = {
+  BASE: "1.125rem",
+  SMALL: "1.5rem",
+  MEDIUM: "2rem",
+  LARGE : "5rem",
+}
+
+const FONT = {
+  FONT_WEIGHT,
+  FONT_SIZE
+}
+
+export { FONT }
+
