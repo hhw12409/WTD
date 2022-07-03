df67c09633c6c26550c3f98ebff7e61393b92827
hhw12409 <sksk2425@naver.com>
2022-07-04 02:08:17 +0900

    Docs: edit path

---
+++ frontend/src/constants/path.ts
@@ -0,0 +1,19 @@
+interface IPath {
+  [key: string]:string
+}
+
+const PATH: IPath = {
+  ROOT: '/',
+  HOME: '/home',
+  FREE_BOARD: '/freeboard',
+  LOGIN: '/login',
+  OAUTH: '/oauth',
+  MYPAGE: '/mypage',
+  MY_REVIEWS: '/mypage/reviews',
+  SEARCH: '/search',
+  SEARCH_RESULT: '/search/result',
+  ERROR_PAGE: '/error',
+  NON_EXISTENCE_PAGE: '*',
+};
+
+export default PATH;
