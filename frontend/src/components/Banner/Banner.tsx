7636badd0eda84b415cb6e4b6bac5c1b7836508f
hhw12409 <sksk2425@naver.com>
2022-07-08 01:55:11 +0900

    Docs : styled component 세분화 작업

--- frontend/src/components/Banner/Banner.tsx
+++ frontend/src/components/Banner/Banner.tsx
@@ -1,7 +1,5 @@
-import styled from "styled-components";
-import { FONT } from "../../constants/font"
-import { SIZE } from "../../constants/size"
 import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import { BannerBg, BannerBox, BannerWrapper, BannerTitle, BannerContent, BannerHome } from './Banner.styles'
 
 function Banner() {
   return (
@@ -20,61 +18,5 @@ function Banner() {
     </BannerBg>
   );
 }
-
-const BannerBg = styled.div`
-  background-image: url("https://source.unsplash.com/random/");
-  background-size: cover;
-  background-position: center;
-  width: var(--banner-size-width);
-  height: var(--banner-size-height);
-`;
-
-const BannerBox = styled.div`
-  width : 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: var(--banner-size-height);
-`;
-
-const BannerWrapper = styled.div`
-  display: flex;
-  flex-direction : column;
-  width : 50%;
-`
-
-const BannerTitle = styled.div`
-  color: var(--color-white);
-  font-weight: var(--font-weight-small);
-  font-size: var(--font-size-medium);
-  width : 100%;
-  text-align : start;
-  margin-bottom : var(--margin-size-small);
-`;
-
-const BannerContent = styled.div`
-  color : var(--color-white);
-  width : 100%;
-  text-align : start;
-  line-height : var(--line-height-medium)
-`;
-
-const BannerHome = styled.div`
-  font-size : var(--font-size-medium);
-  text-transform : uppercase;
-  border: 1px solid black;
-  background: white;
-  color: black;
-  padding: 12px 10px 12px 25px;
-  border-radius: 20px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-  &:hover {
-    color: white;
-    background: black;
-  }
-`
 
 export default Banner;
--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,6 +1,4 @@
-import styled from "styled-components";
-import { FONT } from "../../constants/font"
-import { SIZE } from "../../constants/size"
+import {Center, Nav, LogoBox, Button, MyPageButton, LoginButton} from './Navbar.styles'
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 
 function Navbar() {
@@ -17,44 +15,5 @@ function Navbar() {
     </Nav>
   );
 }
-
-const Center = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const Nav = styled(Center)`
-  display: flex;
-  justify-content: space-around;
-  width: 100%;
-  margin-top: var(--margin-size-base);
-`;
-
-const LogoBox = styled(Center)`
-  padding: var(--padding-size-base) var(--padding-size-large);
-  background: var(--color-logo-bg);
-  font-size: var(--font-size-small);
-`;
-
-const Button = styled.button`
-  padding: ${SIZE.PADDING_SIZE.SMALL} ${SIZE.PADDING_SIZE.BASE};
-  background: white;
-  border: 1px solid black;
-  border-radius: 10px;
-  cursor: pointer;
-  &:hover {
-    background: black;
-    color: white;
-    font-weight: ${FONT.FONT_WEIGHT.BOLD};
-  }
-`
-
-const MyPageButton = styled(Button)`
-  margin-right: ${SIZE.MARGIN_SIZE.BASE};
-`
-
-const LoginButton = styled(Button)`
-`
 
 export default Navbar;
